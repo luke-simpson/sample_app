d8b62a8b7d896365ea218b2f810eb22737e75e6d
luke.simpson <luke.simpson@liveality.com>
2014-02-24 19:06:25 -0600

    add a StaticPages controller

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe StaticPagesController do  
+  describe "Home page" do
+      it "should have the content 'Sample App'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Sample App')
+      end
+    end
+  config.include Capybara::DSL
+end
