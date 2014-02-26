89c9ebc2191411794bdc40ed3a34b16dae928d68
luke.simpson <luke.simpson@liveality.com>
2014-02-25 21:13:11 -0600

    Exercise 1 complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  # Manual addition as directed by tutorial
+  config.include Capybara::DSL
 end
