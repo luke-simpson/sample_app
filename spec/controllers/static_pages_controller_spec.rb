2b5c2b84ba6e7393148ab426fce8c5ddbd1d8811
Mark McEahern <mark@mceahern.com>
2014-03-29 09:11:43 -0500

    Controller specs don't use Capybara

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe StaticPagesController do  
   describe "Home page" do
-      it "should have the content 'Sample App'" do
-        visit '/static_pages/home'
-        expect(page).to have_content('Sample App')
-      end
+    it "should have the content 'Sample App'" do
+      get :home
+      expect(response).to be_success
     end
   end
 end
