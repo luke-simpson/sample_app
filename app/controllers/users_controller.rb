4e8b8c3cec5fd0f8c394b5438dd909b1940d4d6e
Sara Howden <lhsimpson@madisoncollege.edu>
2014-04-20 16:14:35 -0500

    Up to 8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -18,8 +20,8 @@ class UsersController < ApplicationController
   
   private
 
-     def user_params
-       params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
-     end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
 end
